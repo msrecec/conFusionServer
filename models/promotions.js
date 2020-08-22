124b588e8fea6177e8ad30d48d5e83687a2edb81
Mislav Srecec <mislav.srecec3@gmail.com>
2020-08-22 13:22:20 +0200

    Added featured and timestamp to models

--- models/promotions.js
+++ models/promotions.js
@@ -3,30 +3,39 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
-const promoSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const promoSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    price: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
   },
-  image: {
-    type: String,
-    required: true,
-  },
-  label: {
-    type: String,
-    default: '',
-  },
-  price: {
-    type: Currency,
-    required: true,
-    min: 0,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 let Promotions = mongoose.model('Promotion', promoSchema);
 
