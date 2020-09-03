e2da8991506a742edea3bead60364dd69f84ec7f
Mislav Srecec <mislav.srecec3@gmail.com>
2020-09-03 02:19:53 +0200

    Express Sessions Part 2

--- app.js
+++ app.js
@@ -49,45 +49,25 @@ app.use(
   })
 );
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    let authHeader = req.headers.authorization;
+    let err = new Error('You are not authenticated!');
 
-    if (!authHeader) {
-      let err = new Error('You are not authenticated!');
-
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-
-    let auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
-      .toString()
-      .split(':');
-
-    let username = auth[0];
-    let password = auth[1];
-
-    if (username === 'admin' && password === 'password') {
-      req.session.user = 'admin';
-      next();
-    } else {
-      let err = new Error('You are not authenticated!');
-
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-
-      return next(err);
-    }
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 403;
+    return next(err);
   } else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next();
     } else {
       let err = new Error('You are not authenticated!');
 
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -100,8 +80,6 @@ app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 //adding routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
--- routes/users.js
+++ routes/users.js
@@ -55,27 +55,39 @@ router.post('/login', (req, res, next) => {
     let password = auth[1];
 
     User.findOne({ username: username })
-    .then((user) => {
-      if(user === null) {
-        let err = new Error(`User ${username} does not exist!`);
-        err.status = 403;
-        return next(err);
-      } else if(user.password !== password) {
-        let err = new Error(`Your password is incorrect!`);
-        err.status = 403;
-        return next(err);
-      } else if (user.username === username && user.password === password) {
-        req.session.user = 'authenticated';
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('You are authenticated!');
-      }
-    })
-    .catch((err) => next(err));
+      .then((user) => {
+        if (user === null) {
+          let err = new Error(`User ${username} does not exist!`);
+          err.status = 403;
+          return next(err);
+        } else if (user.password !== password) {
+          let err = new Error(`Your password is incorrect!`);
+          err.status = 403;
+          return next(err);
+        } else if (user.username === username && user.password === password) {
+          req.session.user = 'authenticated';
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('You are authenticated!');
+        }
+      })
+      .catch((err) => next(err));
   } else {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('You are already authenticated');
+  }
+});
+
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  } else {
+    let err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
   }
 });
 
