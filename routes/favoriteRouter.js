fb607f61aa6a8343368d73c1fe8c841a38ede5c9
Mislav Srecec <mislav.srecec3@gmail.com>
2020-09-08 17:02:02 +0200

    Added favoriteRouter /favorite  route

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -26,68 +26,99 @@ favoriteRouter
   .options(cors.corsWithOptions, (req, res) => {
     res.sendStatus(200);
   })
-  .get(
-    cors.corsWithOptions,
-    authenticate.verifyUser,
-    authenticate.verifyAdmin,
-    (req, res, next) => {
-      const userId = req.decoded._id ? req.decoded._id : req.user._id;
-      Favorites.findOne({
-        postAuthor: userId,
-      })
-        .populate(populateQuery)
-        .then(
-          (favorites) => {
+  .all(cors.corsWithOptions, authenticate.verifyUser)
+  .get((req, res, next) => {
+    const userId = req.decoded._id ? req.decoded._id : req.user._id;
+    Favorites.findOne({
+      postAuthor: userId,
+    })
+      .populate(populateQuery)
+      .then(
+        (fav) => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(fav);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    const userId = req.decoded._id ? req.decoded._id : req.user._id;
+    const dishId = req.body._id;
+    Favorites.findOne({ postAuthor: userId }, (err, fav) => {
+      if (err) {
+        next(err);
+      }
+      if (fav === null) {
+        Favorites.create({}, (err, fav) => {
+          if (err) {
+            next(err);
+          }
+          fav.postAuthor = userId;
+          fav.dishes.push(req.body._id); //dish id
+          fav.save(function (err, fav) {
+            if (err) {
+              next(err);
+            }
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
-            res.json(favorites);
+            res.json(fav);
+          });
+        });
+      } else {
+        Favorites.findOne(
+          {
+            postedBy: {
+              _id: userId,
+            },
           },
-          (err) => next(err)
-        )
-        .catch((err) => next(err));
-    }
-  )
-  .post(
-    cors.corsWithOptions,
-    authenticate.verifyUser,
-    authenticate.verifyAdmin,
-    (req, res, next) => {
-      Favorites.create(req.body)
-        .then(
-          (favorites) => {
-            console.log('Favorites Created ', favorites);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(favorites);
-          },
-          (err) => next(err)
-        )
-        .catch((err) => next(err));
-    }
-  )
-  .put(
-    cors.corsWithOptions,
-    authenticate.verifyUser,
-    authenticate.verifyAdmin,
-    (req, res, next) => {
-      res.statusCode = 403;
-      res.end(`PUT operation not supported on /favorites`);
-    }
-  )
-  .delete(
-    cors.corsWithOptions,
-    authenticate.verifyUser,
-    authenticate.verifyAdmin,
-    (req, res, next) => {
-      Favorites.remove({})
-        .then(
-          (resp) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(resp);
-          },
-          (err) => next(err)
-        )
-        .catch((err) => next(err));
-    }
-  );
+          (err, fav) => {
+            if (err) {
+              next(err);
+            }
+            let index = fav.favoriteDishes.indexOf(dishId);
+            if (index === -1) {
+              fav.favoriteDishes.push(dishId);
+              fav.save((err, fav) => {
+                if (err) {
+                  next(err);
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(fav);
+              });
+            } else {
+              res.statusCode = 409;
+              res.end(`Dish has already been added!`);
+            }
+          }
+        );
+      }
+    });
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /favorites`);
+  })
+  .delete((req, res, next) => {
+    const userId = req.decoded._id ? req.decoded._id : req.user._id;
+    Favorites.findOneAndRemove(
+      {
+        postAuthor: {
+          _id: userId,
+        },
+      },
+      (err, fav) => {
+        if (err) {
+          return next(err);
+        }
+        res.json(fav);
+      }
+    );
+  });
+
+// favoriteRouter.route('/:dishId')
+// .all(cors.corsWithOptions, authenticate.verifyUser)
+
+module.exports = favoriteRouter;
